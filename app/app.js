52bb8cb6c14b95cf4677b3a29b2bbd3ea6f1da30
pizzapup <57334884+pizzapup@users.noreply.github.com>
2021-12-14 20:05:04 -0500

    bikepacking

---
+++ app/app.js
@@ -0,0 +1,30 @@
+// var homeContent = `<h1>HOME</h1>`;
+// var aboutContent = `<h1>ABOUT</h1>`;
+// var productsContent = `<h1>PRODUCTS</h1>`;
+// var contactContent = `<h1>CONTACT</h1>`;
+// function initListeners() {
+// $("nav a").click(function(e) {
+// this stores the id from the anchor tag
+// let aID = e.currentTarget.id;
+// let contentID = aID + "Content";
+// console.log(contentID);
+// console.log("a id" + aID)
+// $("#app").html(eval(contentID));
+// CHANGE CONTENT $("#app").html(contentID);
+// $("#app").append(contentID);
+// $("#app").prepend(contentID);
+// if(aID == "cbc") {
+//     // changes background color to red if "cbc" is clicked on
+//     $("nav").css("background-color", "red")
+// }
+
+// });
+// }
+// $(document).ready(function () {
+// document.ready reads all the lines
+// initListeners();
+// $("#app").html(homeContent);
+// alert("hi");
+// console.log("hi there, what are you doing?")
+
+// });
